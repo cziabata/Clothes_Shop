c3d070570c3cb32980232a2bc9a57932eb6f8b15
Viacheslav Yemets <viacheslav.emets@gmail.com>
2021-11-26 07:41:25 +0200

    start to work with header styles

--- src/UI/Header/Header.jsx
+++ src/UI/Header/Header.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 import styles from "./Header.module.scss";
 import Logo from "./img/Main_Logo.png";
+import CartLogo from "./img/Empty_Cart.png";
+import Currency from "./img/Currency.png";
 
 class Header extends React.Component {
 
@@ -9,18 +11,22 @@ class Header extends React.Component {
         return(
             <header className={styles.header}>
                <div className={styles.headerNavbar}>
-                    <div>
-                        <NavLink to="woman">Woman</NavLink>
+                    <div className={styles.navbarItem}>
+                        <NavLink to="woman">WOMAN</NavLink>
                     </div>
-                    <div>
-                        <NavLink to="men">Men</NavLink>
+                    <div className={styles.navbarItem}>
+                        <NavLink to="men">MEN</NavLink>
                     </div>
-                    <div>
-                        <NavLink to="kids">Kids</NavLink>
+                    <div className={styles.navbarItem}>
+                        <NavLink to="kids">KIDS</NavLink>
                     </div>
                </div>
                <div>
                    <img src={Logo} alt="Main Logo"/>
+               </div>
+               <div className={styles.headerCartBar}>
+                   <div><img src={CartLogo} alt="Cart Logo"/></div>
+                   <div><img src={Currency} alt="Currency Logo"/></div>
                </div>
             </header>
         )
