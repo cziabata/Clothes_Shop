37a6919f2c3cd173b3c10c4b75e76ffec1929b19
Viacheslav Yemets <viacheslav.emets@gmail.com>
2022-01-13 07:21:09 +0200

    refactor graphQL queries - add variables and fragments

--- src/Data_Access_Layer/Data_Access_Layer.js
+++ src/Data_Access_Layer/Data_Access_Layer.js
@@ -1,8 +1,27 @@
 import {gql} from "@apollo/client";
 
+const CATEGORY_DATA = gql`
+fragment categoryData on Category {
+  name, products{name, id, brand, prices{amount, currency}, description, gallery, category, inStock, attributes{name, id, type, items{displayValue, id, value}}}
+}
+`
+
 export const AllData = gql`
+${CATEGORY_DATA}
 query{
-  category{products{name, id, brand, prices{amount, currency}, gallery, category, attributes{name, type, items{value, displayValue, id}}, description}}
+  category{...categoryData}
+}
+`
+export const GetTech = gql`
+${CATEGORY_DATA}
+query{
+  category(input: {title: "tech"}){...categoryData}
+}
+`
+export const GetClothes = gql`
+${CATEGORY_DATA}
+query{
+  category(input: {title: "clothes"}){...categoryData}
 }
 `
 export const GetCategories = gql`
