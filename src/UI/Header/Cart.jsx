16c11e5ed0065f82bf1fce73ada01dfec222f62d
Viacheslav Yemets <viacheslav.emets@gmail.com>
2022-01-19 07:07:15 +0200

    start to work with styles for minicart

--- src/UI/Header/Cart.jsx
+++ src/UI/Header/Cart.jsx
@@ -13,21 +13,42 @@ class ModalCart extends React.Component {
                 <div className={styles.modalContent} onClick={e=>e.stopPropagation()}>
                     <h4>{`My Bag, ${this.props.cartItems.length} items`}</h4>
                     {this.props.cartItems.length>0 
-                        ? this.props.cartItems.map(item=><div>
+                        ? this.props.cartItems.map(item=><div className={styles.cartContainer} id={item.id}>
                             <div>
                                 <div>{item.name}</div>
-                                <div>{item.prices[0].amount}</div>
+                                <div>{item.prices.map(
+                                    price=>{if(price.currency===this.props.currencyName){
+                                        return <span id={price.currency}>{this.props.currentCurrency + price.amount}</span>
+                                    } else {return ""}})}
+                                </div>
                                 <div>
-                                    {item.attributes.map(attribute=><span>{attribute.name}</span>)}
+                                    {
+                                        item.attributes.length > 0 
+                                        ? item.attributes.map(attribute =><div id={attribute.id}>
+                                            <div>{attribute.name}:</div>
+                                            <div className={styles.attrWrapper}>
+                                                {
+                                                    attribute.type === "swatch"
+                                                    ? attribute.items.map(item=><div id={item.id} 
+                                                                                     style={{background:item.value}}
+                                                                                     className={styles.colorAttr}/>)
+                                                    : attribute.items.map(item=><div id={item.id}>{item.value}</div>)
+                                                }
+                                            </div>
+                                        </div>)
+                                        : null
+                                    }
                                 </div>
                             </div>
-                            <div>
-                                <button>+</button>
-                                <div>1</div>
-                                <button>-</button>
-                            </div>
-                            <div>
-                                <img src="" alt="Product in cart"/>
+                            <div className={styles.cartFlexContainer}>
+                                <div className={styles.cartBtns}>
+                                    <button className={styles.cartCounter}>+</button>
+                                    <div>1</div>
+                                    <button className={styles.cartCounter}>-</button>
+                                </div>
+                                <div className={styles.cartItemPhoto}>
+                                    <img src={item.gallery[0]} alt="Product in cart"/>
+                                </div>
                             </div>
                         </div>)
                         : "CART IS EMPTY"
@@ -40,7 +61,9 @@ class ModalCart extends React.Component {
 const mapStateToProps = (state) => {
     return {
         isActiveCart: state.cartReducer.isActiveCart,
-        cartItems: state.cartReducer.cartItems
+        cartItems: state.cartReducer.cartItems,
+        currentCurrency: state.currencyReducer.currentCurrency,
+        currencyName: state.currencyReducer.name,
     }
 }
 export const Cart = connect(mapStateToProps, {deactivateCart})(ModalCart)
